b4f6d94701e822546ab5c9a6bd348190631fe8f8
eddie1101 <erg1101@outlook.com>
2019-09-06 16:04:52 -0400

    fixed parsing errors

--- QuadraticCalc.py
+++ QuadraticCalc.py
@@ -8,15 +8,15 @@ def main():
     constituents = expressions.split(' ')
 
     if len(constituents) == 5:
-        a = constituents[0][0]
-        b = int(constituents[1] + constituents[2])
-        c = int(constituents[3] + constituents[4])
+        a = int(constituents[0][0])
+        b = int(constituents[1][0] + constituents[2][0])
+        c = int(constituents[3][0] + constituents[4])
     elif len(constituents) == 3:
-        a = int(constituents[0])
+        a = int(constituents[0][0])
         b = 0
-        c = int(constituents[1] + constituents[2])
+        c = int(constituents[1][0] + constituents[2])
     elif len(constituents) == 1:
-        a = int(constituents[0])
+        a = int(constituents[0][0])
         b = 0
         c = 0
     else:
@@ -25,11 +25,18 @@ def main():
         c = 0
 
     # x = (-b +/- sqrt(b^2 - 4ac))/2
-    x1 = 0
-    x2 = 0
 
-    x1 = (-b + math.sqrt(b**2 - (4 * a * c))) / 2
-    x2 = (-b - math.sqrt(b**2 - (4 * a * c))) / 2
+    print(str(a) + ' ' + str(b) + ' ' + str(c))
+    print(type(a))
+    print(type(b))
+    print(type(c))
+
+    if (b**2 - (4 * a * c)) < 0:
+        x1 = 'NaN'
+        x2 = 'NaN'
+    else:
+        x1 = (-b + math.sqrt(b**2 - (4 * a * c))) / 2
+        x2 = (-b - math.sqrt(b**2 - (4 * a * c))) / 2
 
     print('bounds: ' + str(x1) + ' and ' + str(x2))
 
