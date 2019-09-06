cbecf15db1e233d3272a69f5d2d00827ca166e0b
eddie1101 <erg1101@outlook.com>
2019-09-06 13:42:51 -0400

    Initial Commit

---
+++ QuadraticCalc.py
@@ -0,0 +1,11 @@
+import math
+
+
+def main():
+    expressions = input("Enter your quadratic expression, separating terms and operators with a space."
+                "\n e.g. '6X^2 - 17000x - 43.679'\n")
+
+    constituents = expressions.split(' ')
+
+if __name__ == "__main__":
+    main()
